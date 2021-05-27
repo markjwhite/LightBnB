de4bbf5c0d4abc72d9adc1e2245ee01656fc7099
markjwhite <markwhite259@gmail.com>
2021-05-27 12:20:53 -0400

    Added pool query for getAllReservations in database.js

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -75,7 +75,16 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+  SELECT *
+  FROM reservations
+  JOIN properties ON property_id = properties.id
+  WHERE guest_id = $1
+  LIMIT $2;
+  `, [guest_id, limit])
+    .then((result) => {
+      return Promise.resolve(result.rows)
+    })
 }
 exports.getAllReservations = getAllReservations;
 
